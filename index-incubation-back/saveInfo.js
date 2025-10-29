11cde874e042303f9bf05d87cc117847517c1a26
unknown <aljohnorpilla07@gmail.com>
2025-10-30 00:29:13 +0800

    fix folder

--- index-incubation-back/saveInfo.js
+++ index-incubation-back/saveInfo.js
@@ -1,4 +1,4 @@
-import { db } from "./api.js";
+import { db } from "../api.js";
 import { collection, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js";
 
 export async function saveApplication(data) {
