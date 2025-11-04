ccbec2644b1d73252360c06a3ef8aea6825cdec9
n7Jong <aljohnorpilla07@gmail.com>
2025-11-04 09:01:32 +0800

    fixed my database

--- index-incubation-back/saveInfo.js
+++ index-incubation-back/saveInfo.js
@@ -1,4 +1,4 @@
-import { db } from "../api.js";
+import { db } from "../index-incubation-back/api.js";
 import { collection, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js";
 
 export async function saveApplication(data) {
